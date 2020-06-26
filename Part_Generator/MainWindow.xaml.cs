6c56584adc2bf75315cc00cb56a78e2ee4e956fb
Shalton91 <31735592+Shalton91@users.noreply.github.com>
2020-06-26 11:54:00 -0500

    Project Start

---
+++ Part_Generator/MainWindow.xaml.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IO = System.IO;
+using SolidEdgeCommunity.Extensions;
+
+namespace Part_Generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<TItem> Master { get; set; } = new List<TItem>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Master = Newtonsoft.Json.JsonConvert.DeserializeObject<List<TItem>>(IO.File.ReadAllText(@"\\rotork.co.uk\files\US-HOUSTON\ENGINEERING\Gears\Aplications\ABPartsGen\PartsConfigurator.json"));
+            
+         
+            GetCBTypes();
+
+        }
+
+        public void GetCBTypes()
+        {
+            List<string> options = new List<string>();
+            options.Add("-");
+            foreach(TItem t  in Master)
+            {
+                if (!options.Contains(t.Type))
+                {
+                    options.Add(t.Type);
+                }
+            }
+            options.Sort();
+            cbType.ItemsSource = options;
+            cbType.SelectedItem = "-";
+        }
+
+        private void cbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<string> options = new List<string>();
+            options.Add("-");
+
+            foreach (TItem t in Master)
+            {
+                if (t.Type == cbType.SelectedItem.ToString())
+                {
+                    if (!options.Contains(t.Size))
+                    {
+                        options.Add(t.Size);
+                    }
+                }
+
+            }
+            options.Sort();
+
+            cbSize.ItemsSource = options;
+            cbSize.SelectedItem = "-";
+            cbHand.ItemsSource= null;
+            cbDuty.ItemsSource =  null;
+            cbMachType.ItemsSource = null;
+            }
+
+        private void cbSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<string> options = new List<string>();
+            options.Add("-");
+
+            if(cbType.SelectedItem == null || cbSize.SelectedItem == null)
+            {
+                return;
+            }
+            foreach (TItem t in Master)
+            {
+                if (t.Type == cbType.SelectedItem.ToString() && t.Size == cbSize.SelectedItem.ToString())
+                {
+                    if (!options.Contains(t.Hand))
+                    {
+                        options.Add(t.Hand);
+                    }
+                }
+
+            }
+            options.Sort();
+            cbHand.ItemsSource = options;
+            cbHand.SelectedItem = "-";
+            cbDuty.ItemsSource =  null;
+            cbMachType.ItemsSource =  null;
+            
+        }
+
+        private void cbHand_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<string> options = new List<string>();
+            options.Add("-");
+
+            foreach (TItem t in Master)
+            {
+                if (t.Type == cbType.SelectedItem.ToString() &&
+                    t.Size == cbSize.SelectedItem.ToString() && 
+                    t.Hand == cbHand.SelectedItem.ToString())
+                {
+                    if (!options.Contains(t.Duty))
+                    {
+                        options.Add(t.Duty);
+                    }
+                }
+
+            }
+            options.Sort();
+            cbDuty.ItemsSource = options;
+            cbDuty.SelectedItem = "-";
+            cbMachType.ItemsSource = null;
+        }
+
+        private void cbDuty_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<string> options = new List<string>();
+            options.Add("-");
+
+            foreach (TItem t in Master)
+            {
+                if (t.Type == cbType.SelectedItem.ToString() &&
+                    t.Size == cbSize.SelectedItem.ToString() &&
+                    t.Hand == cbHand.SelectedItem.ToString() && 
+                    t.Duty == cbDuty.SelectedItem.ToString())
+                {
+                    if (!options.Contains(t.Mach_Type))
+                    {
+                        options.Add(t.Mach_Type);
+                    }
+                }
+
+            }
+            options.Sort();
+            cbMachType.ItemsSource = options;
+            cbMachType.SelectedItem = "-";
+
+        }
+
+        private void cbMachType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<TItem> options = new List<TItem>();
+            foreach (TItem t in Master)
+            {
+                if (t.Type == cbType.SelectedItem.ToString() &&
+                    t.Size == cbSize.SelectedItem.ToString() &&
+                    t.Hand == cbHand.SelectedItem.ToString() &&
+                    t.Duty == cbDuty.SelectedItem.ToString() &&
+                    t.Mach_Type == cbMachType.SelectedItem.ToString())
+                {
+                    options.Add(t);
+
+
+                }
+
+            }
+            try
+            {
+
+            options.Sort();
+            }
+            catch { }
+   cbGroup.ItemsSource = options;
+        }
+
+        private void Tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           TextBox tb = (TextBox)sender;
+            defualtAtts da = (defualtAtts)tb.Tag;
+            da.Default_Value = tb.Text;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TItem TagItem = ((TItem)cbGroup.SelectedItem);
+            //MessageBox.Show(TagItem.Default_attributes[0].Default_Value);
+            mTMSLibrary.Screens m = new mTMSLibrary.Screens("72");
+            tbResults.Text += TagItem.AddToConfigurator(m, tbPartNumber.Text)+ "\n";
+            foreach (defualtAtts da in TagItem.Default_attributes)
+            {
+               tbResults.Text +=  da.MCO61(m, TagItem, tbPartNumber.Text) + "\n";
+            }
+            m.Close();
+            m = new mTMSLibrary.Screens("13");
+            tbResults.Text += TagItem.AddToConfigurator(m, tbPartNumber.Text) + "\n";
+            foreach (defualtAtts da in TagItem.Default_attributes)
+            {
+                tbResults.Text += da.MCO61(m, TagItem, tbPartNumber.Text) + "\n";
+            }
+
+
+        }
+
+        
+
+        private void cbGroup_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TItem T = (TItem)cbGroup.SelectedItem ;
+            spAtts.Children.Clear();
+
+
+            foreach (defualtAtts da in T.Default_attributes)
+            {
+                TextBlock tbl = new TextBlock();
+                tbl.Text = da.Att_Name;
+                tbl.MinWidth = 75;
+                TextBox tb = new TextBox();
+                tb.Text = da.Default_Value;
+                tb.Tag = da;
+                tb.TextChanged += Tb_TextChanged;
+
+
+                DockPanel sp = new DockPanel();
+                sp.Margin = new Thickness(5);
+                sp.Children.Add(tbl);
+                sp.Children.Add(tb);
+                spAtts.Children.Add(sp);
+            }
+
+            return;
+        }
+        public string RandString()
+        {
+            DateTime dt = DateTime.Now;
+            return dt.Year.ToString() + dt.Month.ToString() + dt.Day.ToString() + dt.Hour.ToString() + dt.Minute.ToString() + dt.Second.ToString() + dt.Millisecond.ToString();
+
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            TItem TagItem = ((TItem)cbGroup.SelectedItem);
+            Console.WriteLine(TagItem.SolidEdgePart.FilePath);
+            if (!string.IsNullOrEmpty(TagItem.SolidEdgePart.FilePath))
+            {
+                Console.WriteLine("Yes");
+                switch (TagItem.Mach_Type)
+                {
+                    case "DD":
+                        Console.WriteLine("Yes");
+                        SolidEdgeDD(TagItem);
+                        break;
+                    default:
+                        MessageBox.Show("This type has not been implemented yet");
+                        break;
+                }
+            }
+
+
+        }
+
+        public void SolidEdgeDD(TItem TagItem)
+        {   //random string for saving the new files         
+            string tempname = RandString();
+            //Across
+            double AF = double.Parse(TagItem.GetAttByName("DDAFLATS").Default_Value) / 1000.0;
+            double drillDia = Math.Round(AF * 375.0, 0) / 1000.0;
+            double depth = AF * 1.5;
+            double DDround = double.Parse(TagItem.GetAttByName("DDROUND").Default_Value) / 1000.0;
+            double CboreDia = DDround + .0002;
+            double hold = Math.Sqrt(Math.Pow(DDround, 2.0) - Math.Pow(AF, 2.0)) + drillDia;
+
+            if (hold > DDround) { DDround = hold; }
+
+            SolidEdgeCommunity.OleMessageFilter.Register();
+            SolidEdgeFramework.Application application = SolidEdgeCommunity.SolidEdgeUtils.Connect(true,true);
+            SolidEdgePart.PartDocument partDocument = application.Documents.Open<SolidEdgePart.PartDocument>(TagItem.SolidEdgePart.FilePath + ".par");
+            
+            //update properties
+            Dictionary<string, string> Props = new Dictionary<string, string>();
+            string IDesc = TagItem.Type;
+            IDesc += ", " + TagItem.Model + TagItem.Size;
+            IDesc += " " + TagItem.Duty;
+            IDesc += " DD" + TagItem.GetAttByName("DDAFLATS").Default_Value;
+            IDesc += " Ø" + TagItem.GetAttByName("DDROUND").Default_Value;
+            Props.Add("Title", IDesc);
+            VariablesHelper.UpdatePrperties((SolidEdgeFramework.SolidEdgeDocument)partDocument, Props);
+            
+           //Update machining dimensions
+            Dictionary<string, double> dimensions = new Dictionary<string, double>();
+            dimensions.Add("DDAFlats", AF);
+            dimensions.Add("DDRFlats", DDround);
+            dimensions.Add("DDCornerRadious", drillDia / 2.0);
+            dimensions.Add("DD_FiniteDepth", depth);
+            dimensions.Add("CBoreDia", CboreDia);
+            VariablesHelper.UpdateDimensions((SolidEdgeFramework.SolidEdgeDocument)partDocument, dimensions);
+
+            //Save Part File
+            partDocument.SaveAs( @"C:\myloadpoint\" + tempname + ".par");
+            //Open Draft file
+            SolidEdgeDraft.DraftDocument draftDocument = application.Documents.Open<SolidEdgeDraft.DraftDocument>(TagItem.SolidEdgePart.FilePath + ".dft");
+            //Set Link to saved part file
+            var m = draftDocument.ModelLinks;
+            m.Item(1).ChangeSource(@"C:\myloadpoint\" + tempname + ".par");
+            //update draft's views to show new linked part file
+            VariablesHelper.UpdateDrawingViews(draftDocument);
+            //save draft file
+            draftDocument.SaveAs(@"C:\myloadpoint\" + tempname + ".dft");
+        }
+    }
+
+    public class TItem
+    {
+        public string GroupName { get; set; }
+        public string Type { get; set; }
+        public string Size { get; set; }
+        public string Hand { get; set; }
+        public string Duty { get; set; }
+        public string Mach_Type { get; set; }
+        public string MTMS_Item { get; set; }
+        public string MTMS_Stage { get; set; }
+        public string Model { get; set; }
+        public SolidEdgeItem SolidEdgePart { get; set; } = new SolidEdgeItem();
+
+        public List<defualtAtts> Default_attributes { get; set; } = new List<defualtAtts>();
+
+        public string AddToConfigurator(mTMSLibrary.Screens m,  string Part_Number)
+        {
+            return (string)m.MCO52(Part_Number,Model,GroupName,1,MTMS_Stage,MTMS_Item);
+
+        }
+        public override string ToString()
+        {
+            return GroupName;
+        }
+        public defualtAtts GetAttByName (string s)
+        {
+            foreach(defualtAtts d in Default_attributes)
+            {
+                if (s == d.Att_Name)
+                {
+                    return d;
+                }
+            }
+            return null;
+        }
+    }
+    public class defualtAtts
+    {
+        public string Att_Name { get; set; }
+        public int intType { get; set; }
+        public int Att_Type { get; set; }
+        public string Default_Value { get; set; }
+
+        public string MCO61(mTMSLibrary.Screens m, TItem T, string Part_Number)
+        {
+            return (string)m.MCO61(Part_Number, T.Model, T.GroupName, T.MTMS_Stage, T.MTMS_Item, Att_Name, intType, Default_Value, Att_Type);
+             
+        }
+    }
+    public class SolidEdgeItem
+    {
+        public string FilePath { get; set; } = "";
+        public string GearLine { get; set; } = "";
+        public string GearSize { get; set; } = "";
+        public string GearHand { get; set; } = "";
+        public string PartNumber { get; set; } = "";
+        public double PilotHoleDia { get; set; } = 0.0;
+        public double MaxDia { get; set; } = 0.0;
+        public double Height { get; set; } = 0.0;
+    }
+
+
+}
---
+++ Part_Generator/VariablesHelper.cs
@@ -0,0 +1,192 @@
+﻿using SolidEdgeCommunity.Extensions; // https://github.com/SolidEdgeCommunity/SolidEdge.Community/wiki/Using-Extension-Methods
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class VariablesHelper
+{
+    public static void ReportVariables(SolidEdgeAssembly.AssemblyDocument document)
+    {
+        ReportVariables((SolidEdgeFramework.SolidEdgeDocument)document);
+    }
+
+    public static void ReportVariables(SolidEdgeDraft.DraftDocument document)
+    {
+        ReportVariables((SolidEdgeFramework.SolidEdgeDocument)document);
+    }
+
+    public static void ReportVariables(SolidEdgePart.PartDocument document)
+    {
+        ReportVariables((SolidEdgeFramework.SolidEdgeDocument)document);
+    }
+
+    public static void ReportVariables(SolidEdgePart.SheetMetalDocument document)
+    {
+        ReportVariables((SolidEdgeFramework.SolidEdgeDocument)document);
+    }
+
+    public static void ReportVariables(SolidEdgePart.WeldmentDocument document)
+    {
+        ReportVariables((SolidEdgeFramework.SolidEdgeDocument)document);
+    }
+
+    public static void ReportVariables(SolidEdgeFramework.SolidEdgeDocument document)
+    {
+        SolidEdgeFramework.Variables variables = null;
+        SolidEdgeFramework.VariableList variableList = null;
+        SolidEdgeFramework.variable variable = null;
+        SolidEdgeFrameworkSupport.Dimension dimension = null;
+
+        if (document == null) throw new ArgumentNullException("document");
+
+        // Get a reference to the Variables collection.
+        variables = (SolidEdgeFramework.Variables)document.Variables;
+
+        // Get a reference to the variablelist.
+        variableList = (SolidEdgeFramework.VariableList)variables.Query(
+            pFindCriterium: "*",
+            NamedBy: SolidEdgeConstants.VariableNameBy.seVariableNameByBoth,
+            VarType: SolidEdgeConstants.VariableVarType.SeVariableVarTypeBoth);
+
+        // Process variables.
+        foreach (var variableListItem in variableList.OfType<object>())
+        {
+            // Not used in this sample but a good example of how to get the runtime type.
+            var variableListItemType = SolidEdgeCommunity.Runtime.InteropServices.ComObject.GetType(variableListItem);
+
+            // Use helper class to get the object type.
+            var objectType = SolidEdgeCommunity.Runtime.InteropServices.ComObject.GetPropertyValue<SolidEdgeFramework.ObjectType>(variableListItem, "Type", (SolidEdgeFramework.ObjectType)0);
+
+            // Process the specific variable item type.
+            switch (objectType)
+            {
+                case SolidEdgeFramework.ObjectType.igDimension:
+                    // Get a reference to the dimension.
+                    dimension = (SolidEdgeFrameworkSupport.Dimension)variableListItem;
+                    Console.WriteLine("Dimension: '{0}' = '{1}' ({2})", dimension.DisplayName, dimension.Value, objectType);
+                    break;
+                case SolidEdgeFramework.ObjectType.igVariable:
+                    variable = (SolidEdgeFramework.variable)variableListItem;
+                    Console.WriteLine("Variable: '{0}' = '{1}' ({2})", variable.DisplayName, variable.Value, objectType);
+                    break;
+                default:
+                    // Other SolidEdgeConstants.ObjectType's may exist.
+                    break;
+            }
+        }
+    }
+
+    public static void UpdateDimensions(SolidEdgeFramework.SolidEdgeDocument document, Dictionary<string,double> DimName)
+    {
+        SolidEdgeFramework.Variables variables = null;
+        SolidEdgeFramework.VariableList variableList = null;
+        SolidEdgeFrameworkSupport.Dimension dimension = null;
+
+        if (document == null) throw new ArgumentNullException("document");
+
+        // Get a reference to the Variables collection.
+        variables = (SolidEdgeFramework.Variables)document.Variables;
+
+        // Get a reference to the variablelist.
+        variableList = (SolidEdgeFramework.VariableList)variables.Query(
+            pFindCriterium: "*",
+            NamedBy: SolidEdgeConstants.VariableNameBy.seVariableNameByBoth,
+            VarType: SolidEdgeConstants.VariableVarType.SeVariableVarTypeBoth);
+
+        // Process variables.
+        foreach (var variableListItem in variableList.OfType<object>())
+        {
+            // Not used in this sample but a good example of how to get the runtime type.
+            var variableListItemType = SolidEdgeCommunity.Runtime.InteropServices.ComObject.GetType(variableListItem);
+
+            // Use helper class to get the object type.
+            var objectType = SolidEdgeCommunity.Runtime.InteropServices.ComObject.GetPropertyValue<SolidEdgeFramework.ObjectType>(variableListItem, "Type", (SolidEdgeFramework.ObjectType)0);
+
+            //Process the specific variable item type.
+
+            if (objectType == SolidEdgeFramework.ObjectType.igDimension)
+            {
+                dimension = (SolidEdgeFrameworkSupport.Dimension)variableListItem;
+                if (DimName.Keys.Contains(dimension.DisplayName))
+                {
+                    dimension.Value = DimName[dimension.DisplayName];
+                    
+                }
+
+            }
+        }
+    }
+    public static void UpdatePrperties(SolidEdgeFramework.SolidEdgeDocument document, Dictionary<string, string> PropName)
+    {
+        var propertySets = (SolidEdgeFramework.PropertySets)document.Properties;
+        foreach (var properties in propertySets.OfType<SolidEdgeFramework.Properties>())
+        {
+            Console.WriteLine(properties.Name);
+            foreach (var property in properties.OfType<SolidEdgeFramework.Property>())
+            {
+                Console.WriteLine("     " + property.Name);
+                if (PropName.Keys.Contains(property.Name))
+                {
+                    property.set_Value(PropName[property.Name]);
+                }
+            }
+        }
+
+    }
+    public static void UpdateDrawingViews(SolidEdgeDraft.DraftDocument draftDocument)
+    {
+        SolidEdgeDraft.Sections sections = null;
+        SolidEdgeDraft.Section section = null;
+        SolidEdgeDraft.SectionSheets sectionSheets = null;
+        SolidEdgeDraft.DrawingViews drawingViews = null;
+        try
+        {
+            if (draftDocument != null)
+            {
+                // Get a reference to the Sections collection.
+                sections = draftDocument.Sections;
+
+                // Get a reference to the WorkingSection.
+                section = sections.WorkingSection;
+
+                // Get a reference to the Sheets collection.
+                sectionSheets = section.Sheets;
+
+                foreach (var sheet in sectionSheets.OfType<SolidEdgeDraft.Sheet>())
+                {
+                    Console.WriteLine("Processing sheet '{0}'.", sheet.Name);
+
+                    // Get a reference to the DrawingViews collection.
+                    drawingViews = sheet.DrawingViews;
+
+                    foreach (var drawingView in drawingViews.OfType<SolidEdgeDraft.DrawingView>())
+                    {
+                        // Updates an out-of-date drawing view.
+                        drawingView.Update();
+
+                        // Note: You can use ForceUpdate() even if it is not out-of-date.
+
+                        Console.WriteLine("Updated drawing view '{0}'.", drawingView.Name);
+                    }
+                }
+            }
+            else
+            {
+                throw new System.Exception("No active document.");
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            SolidEdgeCommunity.OleMessageFilter.Unregister();
+        }
+    }
+    public bool IsH11(int Nom, int Min, int Max)
+    {
+
+    }
+}
